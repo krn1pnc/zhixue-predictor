2a2a70bdd7ad5c0698a1f0ae2a5191bcb917bdc1
krn1pnc <krn1pnc@outlook.com>
2024-08-25 09:14:10 +0800

    init

---
+++ zhixue-predictor.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name        Zhixue Predictor
+// @namespace   krn1pnc
+// @version     1.0
+// @description 在智学网中预测排名
+// @icon        https://static.zhixue.com/zhixue.ico
+// @author      krn1pnc
+// @match       https://www.zhixue.com/activitystudy/web-report/index.html*
+// ==/UserScript==
+
+(function() {
+    "use strict";
+
+    XMLHttpRequest.prototype._open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+        if (url.indexOf("https://www.zhixue.com/zhixuebao/report/exam/getLevelTrend") !== -1 ||
+            url.indexOf("https://www.zhixue.com/zhixuebao/report/paper/getLevelTrend") !== -1) {
+            this.addEventListener("load", function(e) {
+                const xhr = e.currentTarget;
+                if (xhr.readyState != 4) return;
+
+                const res = JSON.parse(xhr.response);
+                if (res.errorCode) return;
+
+                let rankListHtml = "";
+                for (let score of res.result.list) {
+                    let levelData = score.levelList.find((x) => x.name === score.improveBar.levelScale);
+                    let upperBound = levelData.upperBound, lowerBound = levelData.lowBound;
+                    let total = score.totalNum, offset = score.improveBar.offset;
+                    let rank = total * ((100 - offset) * upperBound + offset * lowerBound) / 10000;
+                    rankListHtml +=
+                        `<div class="sub-item">
+                            <div class="subject">${score.tag.name}</div>
+                            <div style="margin-left: 150px;">
+                                <div class="bold-level">
+                                    <span class="blue">${rank}</span>
+                                    <span class="specific ml_10">/ ${total}</span>
+                                </div>
+                            </div>
+                        </div>`;
+                }
+
+                document
+                    .querySelector("#report > div > div.report > div > div.report-content > div.hierarchy")
+                    .insertAdjacentHTML("afterend",
+                        `<div class="hierarchy">
+                            <div>
+                                <div class="general"><h2 class="class-a-title">估算排名</h2></div>
+                                <div class="single">${rankListHtml}</div>
+                            </div>
+                        </div>`);
+            });
+        }
+        this._open(method, url, async, user, password);
+    }
+})();
